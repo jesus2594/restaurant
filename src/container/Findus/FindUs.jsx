1c283f976fd27966dc0cd07fca4559d3f6bd8b4a
Jesus Marmol <jesusdavid890@hotmail.com>
2023-02-10 00:30:09 -0500

    Add footer section

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -13,7 +13,7 @@ const FindUs = () => (
           <p className='p__opensans'>Mon - Fri: 10:00 am - 02:00 am</p>
           <p className='p__opensans'>Sat - Sun: 10:00 am - 03:00 am</p>
         </div>
-        <button className='custom_button' style={{ marginTop: '2rem'}}>Visit Us</button>
+        <button type='button' className='custom__button' style={{ marginTop: '2rem'}}>Visit Us</button>
     </div>
 
     <div className='app__wrapper_img'>
